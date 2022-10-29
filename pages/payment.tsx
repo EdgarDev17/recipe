38a4cf779190f972ab861bee9f6b2f847c6775c8
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-10-28 18:50:03 -0600

    removing square payment

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,66 +1,68 @@
-import {PaymentForm, CreditCard} from 'react-square-web-payments-sdk'
-import Toast from "../components/toast-icon";
-import {useState} from "react";
-
+// import {PaymentForm, CreditCard} from 'react-square-web-payments-sdk'
+import Toast from '../components/toast-icon'
+import { useState } from 'react'
 
 const Payment = () => {
+	// const [paymentChecked, setPaymentChecked] = useState(false)
+	// const [errorToast, setErrorToast] = useState(false)
+	// const [successToast, setSuccessToast] = useState(false)
 
-    const [paymentChecked, setPaymentChecked] = useState(false)
-    const [errorToast, setErrorToast] = useState(false)
-    const [successToast, setSuccessToast] = useState(false)
+	// const handleSuccessToast = () => {
+	//     setSuccessToast(false)
+	// }
 
+	// const handleErrorToast = () => {
+	//     setErrorToast(false)
+	// }
 
-    const handleSuccessToast = () => {
-        setSuccessToast(false)
-    }
+	// return (
+	//     <div>
+	//         <div className={'h-screen flex flex-col items-center justify-center'}>
+	//             {errorToast &&
+	//                 <Toast message={'Error al realizar el pago'} handleOnClick={handleErrorToast} variant={'error'}
+	//                        color={'red'}/>}
+	//             {successToast &&
+	//                 <Toast message={'Pago realizado con éxito'} handleOnClick={handleSuccessToast} variant={'success'}
+	//                        color={'green'}/>}
+	//             <PaymentForm
+	//                 applicationId={process.env.NEXT_PUBLIC_APP_ID}
+	//                 locationId={process.env.NEXT_PUBLIC_LOCATION_ID}
+	//                 cardTokenizeResponseReceived={async (token, buyer) => {
+	//                     const response = await fetch('/api/pay', {
+	//                         method: 'POST',
+	//                         headers: {
+	//                             'Content-type': 'application/json',
+	//                             'Authorization': 'Bearer ' + process.env.NEXT_PUBLIC_SQUARE_ACCESS_TOKEN,
+	//                         },
+	//                         body: JSON.stringify({
+	//                             sourceId: token.token,
+	//                             locationId: process.env.NEXT_PUBLIC_LOCATION_ID
+	//                         })
+	//                     })
 
-    const handleErrorToast = () => {
-        setErrorToast(false)
-    }
+	//                     console.log(response)
+	//                     if (response.ok) {
+	//                         setPaymentChecked(true)
+	//                         setSuccessToast(true)
+	//                     } else {
+	//                         setPaymentChecked(false)
+	//                         setErrorToast(true)
+	//                     }
 
-    return (
-        <div>
-            <div className={'h-screen flex flex-col items-center justify-center'}>
-                {errorToast &&
-                    <Toast message={'Error al realizar el pago'} handleOnClick={handleErrorToast} variant={'error'}
-                           color={'red'}/>}
-                {successToast &&
-                    <Toast message={'Pago realizado con éxito'} handleOnClick={handleSuccessToast} variant={'success'}
-                           color={'green'}/>}
-                <PaymentForm
-                    applicationId={process.env.NEXT_PUBLIC_APP_ID}
-                    locationId={process.env.NEXT_PUBLIC_LOCATION_ID}
-                    cardTokenizeResponseReceived={async (token, buyer) => {
-                        const response = await fetch('/api/pay', {
-                            method: 'POST',
-                            headers: {
-                                'Content-type': 'application/json',
-                                'Authorization': 'Bearer ' + process.env.NEXT_PUBLIC_SQUARE_ACCESS_TOKEN,
-                            },
-                            body: JSON.stringify({
-                                sourceId: token.token,
-                                locationId: process.env.NEXT_PUBLIC_LOCATION_ID
-                            })
-                        })
+	//                     let paymentData = JSON.stringify(await response.json(), null, 2)
+	//                     console.log(paymentData)
+	//                 }}>
+	//                 <CreditCard/>
+	//             </PaymentForm>
+	//         </div>
+	//     </div>
+	// )
 
-
-                        console.log(response)
-                        if (response.ok) {
-                            setPaymentChecked(true)
-                            setSuccessToast(true)
-                        } else {
-                            setPaymentChecked(false)
-                            setErrorToast(true)
-                        }
-
-                        let paymentData = JSON.stringify(await response.json(), null, 2)
-                        console.log(paymentData)
-                    }}>
-                    <CreditCard/>
-                </PaymentForm>
-            </div>
-        </div>
-    )
+	return (
+		<>
+			<div>development</div>
+		</>
+	)
 }
 
 export default Payment
