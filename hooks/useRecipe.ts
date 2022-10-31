6d5dc315c249a4b403389992895e7c3eeb5fae91
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-10-31 01:56:35 -0600

    adding recipe custom hook

--- hooks/useRecipe.ts
+++ hooks/useRecipe.ts
@@ -1,5 +1,41 @@
-const useRecipe = () =>{
-    
-}
+import { useEffect, useState } from 'react'
 
-export default useRecipe
+export function useRecipe() {
+	const [recipesList, setRecipesList] = useState([{}])
+	const [queryRecipe, setQueryRecipe] = useState('')
+	const [searchedList, setSearchedList] = useState()
+
+	useEffect(() => {
+		fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.NEXT_PUBLIC_API_KEY}&number=10`
+		)
+			.then((response) => {
+				return response.json()
+			})
+			.then((data) => {
+				setRecipesList(data.recipes)
+			})
+	}, [])
+
+
+	useEffect(() => {
+		fetch(
+			`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_API_KEY}&query=${queryRecipe}`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setSearchedList(data.results)
+			})
+	}, [queryRecipe])
+
+	function handleSearch(queryParam) {
+		setQueryRecipe(queryParam)
+	}
+
+	return {
+		recipesList,
+        searchedList,
+        queryRecipe,
+        handleSearch,
+	}
+}
